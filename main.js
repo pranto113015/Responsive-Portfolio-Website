896f0a86261778b57445fb87b24bf591daf6b655
Your Name <pranto3876@gmail,com>
2024-01-10 15:03:31 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Coding by Pranto Kumar
+
+var typed = new Typed('.text', {
+    strings: ["Web Designer", "Graphics Designer", "Typing Expert"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
